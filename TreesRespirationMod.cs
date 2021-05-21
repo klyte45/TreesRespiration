10cdda8cfb52367febd4ddb2b377b6803e2572e3
Leandro Klyte <klyte45@hotmail.com>
2021-05-21 04:10:29 -0300

    v2.0.0r4 Removing dialog fallback

--- TreesRespirationMod.cs
+++ TreesRespirationMod.cs
@@ -1,7 +1,7 @@
 using Klyte.Commons.Interfaces;
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.4")]
 namespace Klyte.TreesRespiration
 {
     public class TreesRespirationMod : BasicIUserMod<TreesRespirationMod, TRController, TRPanel>
